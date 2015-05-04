03dc56ff4cfb6a4c71344bf49f6735077c2a8f74
Sérgio Abreu <sergio.abreu@softplan.com.br>
2015-05-04 09:57:42 -0300

    Correction of repos

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -8,7 +8,7 @@ class mongodb::repo::apt inherits mongodb::repo {
   if($::mongodb::repo::ensure == 'present' or $::mongodb::repo::ensure == true) {
     apt::source { 'repo.mongodb.org':
       location    => $::mongodb::repo::location,
-      repos       => 'mongodb-org',
+      repos       => 'mongodb-org/3.0/multiverse',
       key         => '7F0CEB10',
       key_server  => 'hkp://keyserver.ubuntu.com:80',
       include_src => false,
